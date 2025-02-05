a006e365b9f113b0bc85a1d3c3aa7ece02084cc9
Amritansu Aditya <amritansuaditya1@gmail.com>
2025-02-05 09:49:06 +0530

    made 2nd commit

--- CE/file1.js
+++ CE/file1.js
@@ -1,4 +1,8 @@
 const fs = require('fs');    //importing fs module
 
-const data = fs.readFileSync('./data.txt' )
-console.log(data.toString());
+const data = fs.readFileSync('./data.txt', 'utf-8');  //reading data from file
+console.log(data);
+
+fs.appendFileSync('./data.txt', 'Hello World Again!');  //writing data to file
+
+const data2 = fs.readFileSync('./data2.txt', 'utf-8');  //reading data from file
